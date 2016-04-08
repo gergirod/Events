e7d00d33f06278f42ffcd7940a82e9c31325efa2
German Girod <germangirod@gmail.com>
2016-04-08 14:26:03 +1000

    create event model and eventResponse model

---
+++ GoldCoastEvents/app/src/main/java/germangirod/goldcoastevents/data/model/EventResponse.java
@@ -0,0 +1,15 @@
+package germangirod.goldcoastevents.data.model;
+
+import java.util.List;
+
+/**
+ * Created by germangirod on 4/8/16.
+ */
+public class EventResponse {
+
+    public List<Event> events;
+
+    public List<Event> getEvents() {
+        return events;
+    }
+}
